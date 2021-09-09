ec003335b847a11a0b2abf02952d3642b15462ef
dagbouj hatem <dagboujhatem@gmail.com>
2021-09-09 16:23:16 +0100

    add scss & img assets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
-import './App.css';
+import './scss/style.scss';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
